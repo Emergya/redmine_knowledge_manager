d82537c55f449a78b00f3f1264469567a27e99d9
jresinas <jresinas@emergya.com>
2015-05-05 14:29:30 +0200

    Change 'Knowledgements' position

--- init.rb
+++ init.rb
@@ -12,7 +12,8 @@ Redmine::Plugin.register :redmine_knowledge_manager do
   settings :default => {}, :partial => 'settings/km_settings'
   menu :account_menu, :user_km, {:controller => 'user_knowledges', :action => 'show'}, 
   		 :caption => :"km.label_knowledges",
-		   :if => Proc.new { User.current.allowed_to?(:user_km, nil, :global => true) }
+		   :if => Proc.new { User.current.allowed_to?(:user_km, nil, :global => true) }, 
+       :before => :my_account
 
 	menu :admin_menu, :knowledges, { :controller => 'knowledges', :action => 'show'},
   		 :html => { :class => 'issue_statuses' }, 
