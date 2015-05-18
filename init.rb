e42c383a9f5ad48085028dc4b8a8c5c7a862e818
jresinas <jresinas@emergya.com>
2015-05-18 13:14:54 +0200

    Fix plugin permission

--- init.rb
+++ init.rb
@@ -7,12 +7,12 @@ Redmine::Plugin.register :redmine_knowledge_manager do
   description "This plugin allows to store and manage users' knowledge."
   version '0.0.1'
 
-	permission :user_km, { :user_knowledges => [:show] }
+	permission :km_user_knowledges, { :user_knowledges => [:show] }, :require => :loggedin
 
   settings :default => {}, :partial => 'settings/km_settings'
-  menu :account_menu, :user_km, {:controller => 'user_knowledges', :action => 'show'}, 
+  menu :account_menu, :km_user_knowledges, {:controller => 'user_knowledges', :action => 'show'}, 
   		 :caption => :"km.label_knowledges",
-		   :if => Proc.new { User.current.allowed_to?(:user_km, nil, :global => true) }, 
+		   :if => Proc.new { User.current.allowed_to?(:km_user_knowledges, nil, :global => true) }, 
        :before => :my_account
 
 	menu :admin_menu, :knowledges, { :controller => 'knowledges', :action => 'show'},
--- lib/km/hooks.rb
+++ lib/km/hooks.rb
@@ -1,7 +1,7 @@
 module KM
   class Hooks < Redmine::Hook::ViewListener
   	def view_layouts_base_html_head(context={ })
-  		if Setting.plugin_redmine_knowledge_manager['show_alert'].present? and User.current.logged? and User.current.main_knowledges.empty?
+  		if Setting.plugin_redmine_knowledge_manager['show_alert'].present? and User.current.logged? and User.current.main_knowledges.empty? and User.current.allowed_to?(:km_user_knowledges, nil, :global => true)
 	  		return "<div style='overflow:hidden; background-color:#EB0;'>
 	  			<div style='margin:10px; font-weight:bold;'>"+
 	  			l(:"km.text_main_knowledges_alert")+
